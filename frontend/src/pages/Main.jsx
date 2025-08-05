627686c4b1081bd9808c7306565e45b20f7d0b7d
alex <frostell.matters@gmail.com>
2025-08-06 01:37:29 +0300

    feat(router): add routing / and not found

---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,10 @@
+const MainPage = () => {
+  return (
+    <div>
+      <h1 class="text-blue-600 dark:text-sky-400">Главная страница</h1>
+      <p>Добро пожаловать!</p>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Страница не найдена</h1>
+      <p>Извините, запрашиваемая страница не существует.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
