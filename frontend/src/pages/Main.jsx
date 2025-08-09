453a7a7f47fa8fc9929febb6a06c489435404746
alex <frostell.matters@gmail.com>
2025-08-09 23:46:45 +0300

    fix(index.css): remove center body

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -4,7 +4,7 @@ const MainPage = () => {
       <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
         <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">chat</span>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Chat</span>
         </a>
         <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
           <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
