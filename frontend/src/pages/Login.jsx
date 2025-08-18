09f660d096d6a89849ea8b601d576bce9e93c214
alex <frostell.matters@gmail.com>
2025-08-19 00:06:08 +0300

    refactor(Login): add to className cursor-pointer

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
           <ErrorMessage error={error} />
 
           <div>
-            <button type="submit" disabled={status === 'loading'} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <button type="submit" disabled={status === 'loading'} className="cursor-pointer flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               {status === 'loading' ? 'Вход...' : 'Войти'}
             </button>
           </div>
